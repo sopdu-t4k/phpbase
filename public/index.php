1dbeee6fd954bfe7e856d72ac8563fa6371f61bb
Наташа <nata-bakulina82@yandex.ru>
2019-02-12 19:57:29 +0300

    Добавлен ресайз изображений при загрузке

--- public/index.php
+++ public/index.php
@@ -11,8 +11,27 @@ switch ($page) {
     case 'index':
         break;
     case 'gallery':
+        $message = '';
+        if (isset($_POST['load'])) {
+            $filetype = $_FILES['image']['type'];
+            if ($filetype == 'image/png' || $filetype == 'image/jpeg') {
+                $path = './gallery/big/' . $_FILES['image']['name'];
+                if (move_uploaded_file($_FILES['image']['tmp_name'], $path)) {
+                    $img = new SimpleImage();
+                    $img->load($path);
+                    $img->resize(150, 100);
+                    $img->save('./gallery/small/' . $_FILES['image']['name']);
+                    header('Location: ?page=gallery');
+                } else {
+                    $message = 'Ошибка загрузки';
+                }
+            } else {
+                $message = 'Выберите файл с расширением .jpeg или .png';
+            }
+        }
         $params = [
-            'images'=> array_slice(scandir('./gallery/big'), 2),
+            'images' => array_slice(scandir('./gallery/big'), 2),
+            'message' => $message,
         ];
         break;
 }
--- templates/gallery.php
+++ templates/gallery.php
@@ -1,30 +1,17 @@
 <h1>Галерея</h1>
 <div class="clearfix mb-4">
-    <? foreach ($images as $image):?>
+<? foreach ($images as $image): ?>
     <a href="/gallery/big/<?=$image;?>" target="_blank">
-        <img src="/gallery/big/<?=$image;?>" class="img-thumbnail float-left shadow-sm m-1" width="200" />
+        <img src="/gallery/small/<?=$image;?>" class="img-thumbnail float-left shadow-sm m-1" />
     </a>
-    <?endforeach;?>
+<? endforeach; ?>
 </div>
 <h4>Загрузить изображение</h4>
-<?php
-function showAlert($str) {
-    return '<div class="alert alert-danger" role="alert">' . $str . '</div>';
-}
-if (isset($_POST['load'])) {
-    $filetype = $_FILES['image']['type'];
-    if ($filetype == 'image/png' || $filetype == 'image/jpeg') {
-        $path = './gallery/big/' . $_FILES['image']['name'];
-        if (move_uploaded_file($_FILES['image']['tmp_name'], $path)) {
-            header('Location: ?page=gallery');
-        } else {
-            echo showAlert('Ошибка загрузки');
-        }
-    } else {
-        echo showAlert('Выберите файл с расширением .jpeg или .png');
-    }
-}
-?>
+<? if(!empty($message)): ?>
+<div class="alert alert-danger" role="alert">
+    <?=$message;?>
+</div>
+<? endif; ?>
 <form method="post" enctype="multipart/form-data">
     <div class="form-group">
         <input type="file" name="image" class="form-control-file" />
